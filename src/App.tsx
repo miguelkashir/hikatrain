0bd0d4d4e2204f61da32636fceacc0b8b08573e6
Miguel Angel Alvarez <miguelkashir@gmail.com>
2025-04-05 15:14:42 +0200

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,5 @@
-export const App = () => <div>App</div>;
+export const App = () => (
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-3xl">Hikatrain!</h1>
+  </div>
+);
